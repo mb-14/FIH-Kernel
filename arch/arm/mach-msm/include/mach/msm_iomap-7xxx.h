eb460487aaad2068806645f82f078c27d5643a2f
mb-14 <gaglu231094@gmail.com>
2011-12-23 21:33:16 +0530

    add atheros

--- arch/arm/mach-msm/include/mach/msm_iomap-7xxx.h
+++ arch/arm/mach-msm/include/mach/msm_iomap-7xxx.h
@@ -34,7 +34,7 @@
  * changes.
  *
  */
-#ifdef CONFIG_VMSPLIT_2G
+
 #define MSM_VIC_BASE          IOMEM(0xE0000000)
 #define MSM_VIC_PHYS          0xC0000000
 #define MSM_VIC_SIZE          SZ_4K
@@ -99,85 +99,29 @@
 #define MSM_AD5_PHYS          0xAC000000
 #define MSM_AD5_SIZE          (SZ_1M*13)
 
-#else	/* VMSPLIT_3G */
-
-#define MSM_VIC_BASE          IOMEM(0xF8000000)
-#define MSM_VIC_PHYS          0xC0000000
-#define MSM_VIC_SIZE          SZ_4K
-
-#define MSM_CSR_BASE          IOMEM(0xF8001000)
-#define MSM_CSR_PHYS          0xC0100000
-#define MSM_CSR_SIZE          SZ_4K
-
-#define MSM_TMR_PHYS          MSM_CSR_PHYS
-#define MSM_TMR_BASE          MSM_CSR_BASE
-#define MSM_TMR_SIZE          SZ_4K
-
-#define MSM_DMOV_BASE         IOMEM(0xF8002000)
-#define MSM_DMOV_PHYS         0xA9700000
-#define MSM_DMOV_SIZE         SZ_4K
-
-#define MSM_GPIO1_BASE        IOMEM(0xF8003000)
-#define MSM_GPIO1_PHYS        0xA9200000
-#define MSM_GPIO1_SIZE        SZ_4K
-
-#define MSM_GPIO2_BASE        IOMEM(0xF8004000)
-#define MSM_GPIO2_PHYS        0xA9300000
-#define MSM_GPIO2_SIZE        SZ_4K
-
-#define MSM_CLK_CTL_BASE      IOMEM(0xF8005000)
-#define MSM_CLK_CTL_PHYS      0xA8600000
-#define MSM_CLK_CTL_SIZE      SZ_4K
-
-#define MSM_L2CC_BASE         IOMEM(0xF8006000)
-#define MSM_L2CC_PHYS         0xC0400000
-#define MSM_L2CC_SIZE         SZ_4K
-
-#define MSM_SHARED_RAM_BASE   IOMEM(0xF8100000)
-#define MSM_SHARED_RAM_SIZE   SZ_1M
-
-
-#define MSM_UART1_PHYS        0xA9A00000
-#define MSM_UART1_SIZE        SZ_4K
-
-#define MSM_UART2_PHYS        0xA9B00000
-#define MSM_UART2_SIZE        SZ_4K
-
-#define MSM_UART3_PHYS        0xA9C00000
-#define MSM_UART3_SIZE        SZ_4K
-
-#ifdef CONFIG_MSM_DEBUG_UART
-#define MSM_DEBUG_UART_BASE   0xF9000000
-#if CONFIG_MSM_DEBUG_UART == 1
-#define MSM_DEBUG_UART_PHYS   MSM_UART1_PHYS
-#elif CONFIG_MSM_DEBUG_UART == 2
-#define MSM_DEBUG_UART_PHYS   MSM_UART2_PHYS
-#elif CONFIG_MSM_DEBUG_UART == 3
-#define MSM_DEBUG_UART_PHYS   MSM_UART3_PHYS
-#endif
-#define MSM_DEBUG_UART_SIZE   SZ_4K
-#endif
-
-#define MSM_MDC_BASE	      IOMEM(0xF8200000)
-#define MSM_MDC_PHYS	      0xAA500000
-#define MSM_MDC_SIZE	      SZ_1M
-
-#define MSM_AD5_BASE          IOMEM(0xF8300000)
-#define MSM_AD5_PHYS          0xAC000000
-#define MSM_AD5_SIZE          (SZ_1M*13)
-
-#endif /* END VMSPLIT_3G */
-//------------------------------------------------FIH
 #ifdef CONFIG_FIH_FXX
 
+/* FIH, Debbie, 2010/03/04 { */
 #define MSM_PLOG_BASE         IOMEM(0xE1400000)
-#ifdef CONFIG_FIH_PROJECT_FM6 //FM6
+/* FIH, Debbie, 2010/04/16 { */
+/* modify for 4G/4G*/
+//#define MSM_PLOG_PHYS       0x0D600000
+#ifdef CONFIG_FIH_PROJECT_FM6d
+
+/* FM6F */
 #define MSM_PLOG_PHYS       0x12F00000
 #endif
+/* } FIH, Charles Huang, 2010/09/16 */
+/* FIH, Debbie, 2010/04/16 } */
 #define MSM_PLOG_SIZE         SZ_1M
+/* FIH, Debbie, 2010/03/04 } */
+
+/* FIH, Tiger, 2010/3/24 { */
 #define MSM_PLOG_BASE2        IOMEM(0xE1500000)
 #define MSM_PLOG_PHYS2        0x0
+/* FIH, Tiger, 2010/3/24 } */
 
 #endif /* CONFIG_FIH_FXX */
-//-------------------------------------------------FIH
+
 #endif
+
